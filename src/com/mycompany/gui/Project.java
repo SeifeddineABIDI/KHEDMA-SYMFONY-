8656d097d921c6951ce8e3c093ffc2c9aab022e6
unknown <seifeddine.abidi@esprit.tn>
2023-05-10 21:20:47 +0100

    mobile user

---
+++ src/com/mycompany/gui/Project.java
@@ -0,0 +1,26 @@
+package com.mycompany.gui;
+
+import com.codename1.ui.Container;
+import com.codename1.ui.Form;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.util.Resources;
+
+
+public class Project extends Form  {
+    public Project(com.codename1.ui.util.Resources resourceObjectInstance) {
+        initGuiBuilderComponents(resourceObjectInstance);
+    }
+
+//-- DON'T EDIT BELOW THIS LINE!!!
+
+
+// <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initGuiBuilderComponents(com.codename1.ui.util.Resources resourceObjectInstance) {
+        setLayout(new com.codename1.ui.layouts.FlowLayout());
+        setInlineStylesTheme(resourceObjectInstance);
+                setInlineStylesTheme(resourceObjectInstance);
+        setTitle("Project");
+        setName("Project");
+    }// </editor-fold>
+//-- DON'T EDIT ABOVE THIS LINE!!!
+}
